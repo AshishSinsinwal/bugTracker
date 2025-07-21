7aa39a673313a7dbc83778c70de7ff14fb7b86fa
Ashish Sinsinwal <ashishsinsinwal@outlook.com>
2025-07-22 00:37:18 +0530

    frontend connected with backend

--- bug-tracker-frontened/src/api/API.jsx
+++ bug-tracker-frontened/src/api/API.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://bugtracker-7h9g.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
